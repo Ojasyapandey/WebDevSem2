2c0ce434ba34125cd5b1d8c359089bd2206cb8a6
Ojasya Pandey <pandeyojasya@gmail.com>
2026-03-09 09:47:18 +0530

    new

---
+++ firstsemestercode/script.js
@@ -0,0 +1,35 @@
+console.log("hello there");
+document.write("HEllo")
+var num =10;
+console.log(num);
+console.log(typeof(num));
+num= false;
+console.log(num);
+console.log(typeof(num));
+num= "abc";
+console.log(num);
+console.log(typeof(num));
+num= [1,3,5];
+console.log(num);
+console.log(typeof(num));
+num= undefined;
+console.log(num);      
+console.log(typeof(num));
+var hel=function() {
+    alert('sayhello');
+}
+ console.log(hel);
+ 
+  const sym1 = Symbol(4);
+  console.log(sym1);
+
+  const sym2 = Symbol(4);
+  console.log(sym1);
+ if (sym1 == sym2) console.log("true");
+ else console.log("false");
+
+ var a=100 ; var b=13 ; var c = 10; var linebreak="<br>"
+ document.write("a+b+c=");
+result = a+b+c;
+document.write(result);
+document.write(linebreak);
